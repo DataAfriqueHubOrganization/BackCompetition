a9eba26c371637e8e10aca9683b68d69d7b1d38f
haroldhang <haroldadjahi@gmai.com>
2025-04-11 10:36:57 +0100

    settings and deploy

--- settings/settings.py
+++ settings/settings.py
@@ -173,12 +173,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 ########################################
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
